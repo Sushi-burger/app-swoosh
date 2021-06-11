7cfb73c190e8976364db2386df4d7a73d9f5063b
Sunitha Sivasankaran <sunithasivasankaran@Sunithas-MacBook-4.local>
2021-06-11 19:45:02 -0400

    Added Segues

---
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Sunitha Sivasankaran on 6/6/21.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+}
+
+}
